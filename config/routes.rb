af8299d87fd749c48707fa2b82757b8fe60ca9d3
Eon <eoneon123@gmail.com>
2016-11-03 11:01:05 -0700

    added CRUD for bookmarks

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root 'topics#index'
   devise_for :users
   resources :users, only: :show
-  resources :topics
+  resources :topics do
+    resources :bookmarks, except: [:index]
+  end
   get 'about' => 'welcome#about'
   get 'welcome' => 'welcome#index'
 end
