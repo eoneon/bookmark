ff862864617557ac0c02a31ba9094a888f750918
Eon <eoneon123@gmail.com>
2016-11-06 21:00:16 -0800

    added association between users/bookmarks

--- spec/models/bookmark_spec.rb
+++ spec/models/bookmark_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Bookmark, type: :model do
   let(:bookmark) {create(:bookmark)}
   it {is_expected.to belong_to(:topic)}
+  it {is_expected.to belong_to(:user)}
 
   describe "attributes" do
     it "has bookmark to have url attribute" do
