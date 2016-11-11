7617294c6a60f7c8f05e80387bbbdc813c805f95
Eon <eoneon123@gmail.com>
2016-11-11 09:08:43 -0800

    updated users#show and added policy to likes view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     likes = Like.where(user_id: @user.id)
-    @user_liked_bookmarks = Bookmark.where(user_id: likes.pluck(:user_id))
+    @user_liked_bookmarks = Bookmark.where(user_id: @user.id, id: likes.pluck(:bookmark_id))
   end
 end
